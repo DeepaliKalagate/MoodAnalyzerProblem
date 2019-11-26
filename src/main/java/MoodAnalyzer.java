947c83164d69de500add72f11109a2913b98d41f
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-26 10:33:16 +0530

    Added when given Message Sad then should return Sad Mood

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,8 +1,11 @@
 public class MoodAnalyzer
 {
 
-    public String analyzeMood(String s)
+    public String analyzeMood(String message)
     {
+        if (message.contains("Sad"))
         return "SAD";
+        else
+            return "Happy";
     }
 }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -3,11 +3,21 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest
 {
+    //UseCase One Should Return Happy Or Sad Mood
     @Test
     public void giveMessage_WhenSadOrHappy_ShouldReturnSadOrHappy()
     {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String mood = moodAnalyzer.analyzeMood("I am in a Sad Mood");
         Assert.assertEquals("SAD", mood);
+    }
+
+    //UseCase One Should Return Sad Mood
+    @Test
+    public void giveMessage_WhenSad_ShouldReturnSad()
+    {
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+        String mood=moodAnalyzer.analyzeMood("I am in a Sad Mood");
+        Assert.assertEquals("SAD",mood);
     }
 }
